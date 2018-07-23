d7760b0a4f2a9517d708bab6d8c97386f12b804d
Alan Duncan <Alan@Duncans-MacBook-Pro.local>
2018-07-23 01:44:54 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -2,24 +2,24 @@
 
 var cards = [
 	{
-	rank: "queen",
-	suit: "hearts",
-	cardImage: "images/queen-of-hearts.png"
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage: 'images/queen-of-hearts.png'
 	},
 	{
-	rank: "queen",
-	suit: "diamonds",
-	cardImage: "images/queen-of-diamonds.png"
+	rank: 'queen',
+	suit: 'diamonds',
+	cardImage: 'images/queen-of-diamonds.png'
 	},
 	{
-	rank: "king",
-	suit: "hearts",
-	cardImage: "images/king-of-hearts.png"
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: 'images/king-of-hearts.png'
 	},
 	{
-	rank: "king",
-	suit: "diamonds",
-	cardImage: "images/king-of-diamonds.png"
+	rank: 'king',
+	suit: 'diamonds',
+	cardImage: 'images/king-of-diamonds.png'
 	}
 ];
 
@@ -27,6 +27,17 @@ var cards = [
 
 var cardsInPlay = [];
 
+// what card is flipped
+
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
+	checkForMatch();
+};
 
 var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -36,21 +47,18 @@ var checkForMatch = function () {
 	}
 };
 
-// what card is flipped
 
-var flipCard = function (cardId) {
-	console.log("User flipped " + cards[cardId].rank);
-	cardsInPlay.push(cards[cardId].rank);
-	checkForMatch();
-};
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+	var cardElement = document.createElement('img');
+	cardElement.setAttribute('src', 'images/back.png');
+	cardElement.setAttribute('data-id', i);
+	cardElement.addEventListener('click', flipCard);
+	document.getElementById('game-board').appendChild(cardElement);
+	}
+}
 
-
-// call function
-flipCard(0);
-flipCard(2);
-
-console.log(cards[cardId].cardImage);
-console.log(cards[cardId].suit);
+createBoard ();
 
 
 
